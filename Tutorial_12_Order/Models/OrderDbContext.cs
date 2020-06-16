1189431bb5aca939db4cacf4d6a5b255984eb062
s18588 <s18588@pja.edu.pl>
2020-06-16 12:29:18 +0200

    reupload, 1st commit

--- Tutorial_12_Order/Models/OrderDbContext.cs
+++ Tutorial_12_Order/Models/OrderDbContext.cs
@@ -1,7 +1,70 @@
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace Tutorial_12_Order.Models
 {
-    public class OrderDbContext
+    public class OrderDbContext : DbContext
     {
+        public DbSet<Employee> Employee { get; set; }
+
+        public OrderDbContext()
+        {
+        }
+
+        public OrderDbContext(DbContextOptions options) : base(options)
+        {
+        }
         
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Confectionery_Order>()
+                .HasKey(c => new {c.IdConfectionary, c.IdOrder});
+        }
     }
+    
+    public class Employee {
+        
+        [Key] public int IdEmployee { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    public class Customer
+    {
+        [Key] public int IdClient { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Order
+    {
+        [Key] public int IdOrder { get; set; }
+        public DateTime DateAccepted { get; set; }
+        public DateTime DateFinished { get; set; }
+        public string Notes { get; set; }
+        [ForeignKey("Client")] public int IdClient { get; set; }
+        [ForeignKey("Employee")] public int IdEmployee { get; set; }
+    }
+
+    public class Confectionery
+    {
+        [Key] public int IdConfectionery { get; set; }
+        public string Name { get; set; }
+        public float PricePerItem { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Confectionery_Order
+    {
+        [Key] [ForeignKey("Confection")] public int IdConfectionary { get; set; }
+        [Key] [ForeignKey("Order")] public int IdOrder { get; set; }
+        public int quantity { get; set; }
+        public string Notes { get; set; }
+    }
+    
 }
